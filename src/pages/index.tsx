9e350586bb7968b55047e7334b00748b8092cc66
howdyev <phain118@outlook.com>
2023-02-08 03:43:03 -0500

    Before Index page templating

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -96,31 +96,34 @@ const IndexPage: FunctionComponent<IndexPageProps> = function ({
 export default IndexPage
 
 export const getPostList = graphql`
-    query getPostList {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
-      ) {
-        edges {
-          node {
-            id
-            frontmatter {
-              title
-              summary
-              date(formatString: "YYYY.MM.DD.")
-              categories
-              thumbnail {
-                childImageSharp {
-                  gatsbyImageData(width: 768, height: 400)
-                }
+  query getPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData(width: 768, height: 400)
               }
             }
           }
         }
       }
-      file(name: { eq: "profile-image" }) {
-        childImageSharp {
-          gatsbyImageData(width: 120, height: 120)
-        }
+    }
+    file(name: { eq: "profile-image" }) {
+      childImageSharp {
+        gatsbyImageData(width: 120, height: 120)
       }
     }
-  `
+  }
+`
